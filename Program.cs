eb8efab58009fffddf57fc6f8f6be83d7d389519
olimov Amir <olimov.amir@mail.ru>
2023-12-27 11:58:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Extension;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime dateTime = DateTime.Now;
+        dateTime.Print();
+    }
+}
